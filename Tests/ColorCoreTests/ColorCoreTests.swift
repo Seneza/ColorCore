9efaaab4392098ec4045be0319b3858eadaa6724
Gaston Seneza <gseneza@gmail.com>
2020-06-14 19:51:59 -0500

    Provides static variable for color palette

--- Tests/ColorCoreTests/ColorCoreTests.swift
+++ Tests/ColorCoreTests/ColorCoreTests.swift
@@ -2,12 +2,17 @@ import XCTest
 @testable import ColorCore
 
 final class ColorCoreTests: XCTestCase {
-    func testColorRedEqual() {
-        let color = ColorCore.colorFromHexString("FF0000")
-        XCTAssertEqual(color,.red)
+//    func testColorRedEqual() {
+//        let color = ColorCore.colorFromHexString("FF0000")
+//        XCTAssertEqual(color, .red)
+//    }
+    func testColorCoreAreEqual(){
+        let color = ColorCore.colorFromHexString("008976")
+        XCTAssertEqual(color, ColorCore.colorCore)
     }
 
     static var allTests = [
-        ("testColorRedEqual", testColorRedEqual),
+        //("testColorRedEqual", testColorRedEqual),
+        ("testColorCoreAreEqual", testColorCoreAreEqual)
     ]
 }
